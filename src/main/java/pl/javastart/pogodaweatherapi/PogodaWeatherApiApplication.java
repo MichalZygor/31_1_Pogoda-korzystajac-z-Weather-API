7042e689697c4cf5f23d1af9d096b40a16b4b92a
Michał <michal@winami.com>
2021-03-19 18:58:59 +0100

    Starting environment

---
+++ src/main/java/pl/javastart/pogodaweatherapi/PogodaWeatherApiApplication.java
@@ -0,0 +1,13 @@
+package pl.javastart.pogodaweatherapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PogodaWeatherApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PogodaWeatherApiApplication.class, args);
+    }
+
+}
